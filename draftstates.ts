189650cc57a54221d969294fd7cc4a3436dbc437
sureshgds <50654968+sureshgds@users.noreply.github.com>
2025-08-01 20:57:11 +0530

    Create draftstates.ts

---
+++ draftstates.ts
@@ -0,0 +1,22 @@
+export interface IAfkDraftsStates {
+    isSuccess: any;
+    isLoader: any;
+    allDraftIdeaList: any;
+    imageList: any;
+    sK0y: any;
+    isHMAC: any;
+    token: any;
+    lang: any;
+    class: any;
+    englishContent: any;
+    arabicContent: any;
+    ago: any;
+    minute: any;
+    hour: any;
+    day: any;
+    week: any;
+    month: any;
+    year: any;
+    isdraftopopup: any;
+    myideaid: any;
+}
